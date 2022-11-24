7abc8f4f554a76eed7742d7d1d32bafc10297f15
MrHaMzUuu <115740610+MrHaMzUuu@users.noreply.github.com>
2022-11-24 14:45:31 +0500

    initial commit

---
+++ Jawa 5th Assingment/app.js
@@ -0,0 +1,134 @@
+// Changing case
+
+
+// var a = "Karachi";
+
+// var b = prompt("Enter City Name");
+
+// a = a.toLowerCase();
+// a = b.toLowerCase();
+// a = a.toUpperCase();
+// a = b.toUpperCase();
+// if (a == b){
+//     console.log("Same")
+// }
+// var fisTLetter = a.slice(0,1);
+// var otherLetter = a.slice(1);
+// a =  fistLetter .toUpperCase() + otherLetter .toLowerCase();
+// console.log(a);
+
+
+// ================================================================================
+
+
+// Rounding numbers
+
+// var b = a.charat(35);
+// console.log( b)
+// var a = Math.round(1.9);
+
+// console.log(a);
+
+
+// ==============================================================================
+
+
+// Generating random numbers
+
+
+// var a = Math.random();
+// var toss = Math.round(a);
+
+// console.log(a);
+
+// var dice = Math.random();
+// dice = Math.ceil(dice * 6);
+// console.log(dice);
+
+// ===================================================================================
+
+// DATE METHOD
+
+
+//  var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+// var dt = new Date();
+// var a = dt.getMonth ();
+// var a = dt.getDay ();
+// var a = dt.getDate ();
+// var a = dt.getHours ();
+// var a = dt.getMinutes ();
+// var a = dt.getSeconds ();
+// var a = dt.getFullYear ();
+// var a = dt.getMilliseconds ();
+
+// console.log(months[a]);
+
+
+// var dt = new Date();
+
+// dt.setDate(17);
+// dt.setMonth(10);
+// dt.setFullYear(2022);
+// dt.setHours(00);
+// dt.setMinutes(00);
+// dt.setSeconds(00);
+// dt.setMilliseconds(0);
+
+// console.log(dt);
+
+// var  dt1 = new Date();
+// var  dt2 = new Date("01-Dec-2000");
+ 
+// var a = dt1.getTime();
+// var b = dt2.getTime();
+// var c = dt1-dt2.getTime();
+
+// var d = (c/(1000*60*60*24*365))
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var e = Math.round(d);
+
+// console.log("Year = ", e)
+
+
+// =======================================================================================
+
+
+// FUNCTION
+
+
+// function xyz(){
+//     alert("hello")
+// }
+// xyz()
+
+// function calculateDiscount(a,b){
+// var amount = a;
+// var discount = b;
+// var totalamount = amount % discount;
+// console.log(totalamount);
+// }
+ 
+// calculateDiscount(200,2);
+
+
+// var a = 100
+
+// function abc(){
+//     var b = 30
+//     console.log(a)
+// }
+// abc()
+
+// function abc(){
+//     var a = 10;
+// return a;
+// }
+// var c = abc();
+
+// console.log(c);
+
